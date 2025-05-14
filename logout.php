a82ca5e48d2523664de67645d96e30e7ba75e465
ThomasBV1 <70708447+ttvettuparambil@users.noreply.github.com>
2025-05-14 22:17:05 +0530

    secure logout

--- logout.php
+++ logout.php
@@ -49,8 +49,15 @@ if (isset($_SESSION['user_id'])) {
     $stmt->close();
 }
 
+// Clear all session variables
+$_SESSION = array();
+
+// Destroy the session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 3600, '/');
+}
+
 // Destroy the session
-session_unset();
 session_destroy();
 
 // Redirect to login page
